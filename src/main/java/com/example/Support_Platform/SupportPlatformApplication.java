ecb56cda5e8968489b6398f805441e2adf2086b5
nino.aqiashvili <nino.aqiashvili@gmail.com>
2024-09-17 22:45:47 +0400

    add v1

---
+++ src/main/java/com/example/Support_Platform/SupportPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.Support_Platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Support_Platform/SupportPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Support_Platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
